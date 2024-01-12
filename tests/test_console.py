b21ff03aac30315fe6850f07233ad06325db467a
sjamillah <j.ssozi@alustudent.com>
2024-01-12 14:00:12 +0200

    fixed test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Defines unittests for console.py
+"""Defines unittests for console.py.
 Unittest classes:
     TestHBNBCommand_prompting
     TestHBNBCommand_help
     TestHBNBCommand_exit
     TestHBNBCommand_create
     TestHBNBCommand_show
-    TestHBNBCommand_all
     TestHBNBCommand_destroy
+    TestHBNBCommand_all
     TestHBNBCommand_update
 """
+
+
 import os
 import sys
 import unittest
